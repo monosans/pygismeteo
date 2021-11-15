0cd4044a71b9451c5c07d4df8895d241dea4a140
monosans <hsyqixco@protonmail.com>
2021-11-15 22:52:38 +0300

    Fix tests

--- tests/test_gismeteo.py
+++ tests/test_gismeteo.py
@@ -4,9 +4,9 @@ from pygismeteo import gismeteo
 
 def test_locality_id() -> None:
     moscow = gismeteo("https://www.gismeteo.ru/weather-moscow-4368/")
-    assert moscow._base_endpoint == "/weather-moscow-4368/"
+    assert moscow._BASE_ENDPOINT == "/weather-moscow-4368/"
 
 
 def test_locality_name() -> None:
     moscow = gismeteo("Москва")
-    assert moscow._base_endpoint == "/weather-moscow-4368/"
+    assert moscow._BASE_ENDPOINT == "/weather-moscow-4368/"

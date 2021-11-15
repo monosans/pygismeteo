e9cc2ecf3fede874718c1772fc4d7b172d920b2f
monosans <hsyqixco@protonmail.com>
2021-11-15 22:19:04 +0300

    Add types.py

---
+++ pygismeteo/types.py
@@ -0,0 +1,41 @@
+from pygismeteo._class import Gismeteo
+from pygismeteo._dates.abc import ABCDate
+from pygismeteo._dates.month import Month
+from pygismeteo._dates.now import Now
+from pygismeteo._dates.one_day import (
+    In3Days,
+    In4Days,
+    In5Days,
+    In6Days,
+    In7Days,
+    In8Days,
+    In9Days,
+    In10Days,
+    OneDay,
+    Today,
+    Tomorrow,
+)
+from pygismeteo._dates.ten_days import TenDays
+from pygismeteo._dates.three_days import ThreeDays
+from pygismeteo._dates.two_weeks import TwoWeeks
+
+__all__ = (
+    "ABCDate",
+    "Gismeteo",
+    "In10Days",
+    "In3Days",
+    "In4Days",
+    "In5Days",
+    "In6Days",
+    "In7Days",
+    "In8Days",
+    "In9Days",
+    "Month",
+    "Now",
+    "OneDay",
+    "TenDays",
+    "ThreeDays",
+    "Today",
+    "Tomorrow",
+    "TwoWeeks",
+)

b17fb12d38ce0785c694611143cf56810d5cec4d
monosans <hsyqixco@protonmail.com>
2022-01-01 23:46:19 +0300

    Fix linter warning

--- tests/test_exceptions_and_session.py
+++ tests/test_exceptions_and_session.py
@@ -7,6 +7,5 @@ from pygismeteo.exceptions import LocalityNotFound
 
 
 def test_locality_not_found() -> None:
-    with pytest.raises(LocalityNotFound):
-        with Session() as s:
-            pygismeteo.search.id_by_query("волыфдаловыфалдоыфва", session=s)
+    with pytest.raises(LocalityNotFound), Session() as s:
+        pygismeteo.search.id_by_query("волыфдаловыфалдоыфва", session=s)

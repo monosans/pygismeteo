01231f5c8ded054f664aff256f42c8a4f3fb84d5
monosans <hsyqixco@protonmail.com>
2022-04-16 19:03:59 +0300

    Add docstrings

--- pygismeteo/_gismeteo.py
+++ pygismeteo/_gismeteo.py
@@ -8,6 +8,8 @@ from pygismeteo._exceptions import LocalityNotFound
 
 
 class Gismeteo:
+    """Обёртка для Gismeteo.ru API."""
+
     __slots__ = ("lang", "token", "_session")
 
     def __init__(
@@ -17,11 +19,26 @@ class Gismeteo:
         token: str = pygismeteo_base.constants.DEFAULT_TOKEN,
         session: Optional[Session] = None,
     ) -> None:
+        """Обёртка для Gismeteo.ru API.
+
+        Args:
+            lang: язык. По умолчанию "ru".
+            token: X-Gismeteo-Token,
+                если используемый по умолчанию перестал работать.
+            session: экземпляр requests.Session.
+                По умолчанию для каждого запроса создаётся новый экземпляр.
+        """
         self.lang = lang.strip()
         self.token = token.strip()
         self._session = session
 
     def current(self, id: int) -> pygismeteo_base.models.current.Model:
+        """Текущая погода.
+
+        Args:
+            id: ID населённого пункта.
+                Получить можно при помощи метода get_id_by_query.
+        """
         return pygismeteo_base.models.current.Model.parse_obj(
             self._get_response(f"weather/current/{id}")
         )
@@ -29,6 +46,13 @@ class Gismeteo:
     def step3(
         self, id: int, days: pygismeteo_base.types.STEP3_DAYS
     ) -> pygismeteo_base.models.step3or6.Model:
+        """Погода с шагом 3 часа.
+
+        Args:
+            id: ID населённого пункта.
+                Получить можно при помощи метода get_id_by_query.
+            days: Количество дней (от 1 до 10).
+        """
         return pygismeteo_base.models.step3or6.Model.parse_obj(
             self._get_response(f"weather/forecast/{id}", {"days": str(days)})
         )
@@ -36,6 +60,13 @@ class Gismeteo:
     def step6(
         self, id: int, days: pygismeteo_base.types.DAYS
     ) -> pygismeteo_base.models.step3or6.Model:
+        """Погода с шагом 6 часов.
+
+        Args:
+            id: ID населённого пункта.
+                Получить можно при помощи метода get_id_by_query.
+            days: Количество дней (от 3 до 10).
+        """
         return pygismeteo_base.models.step3or6.Model.parse_obj(
             self._get_response(
                 f"weather/forecast/by_day_part/{id}", {"days": str(days)}
@@ -45,6 +76,13 @@ class Gismeteo:
     def step24(
         self, id: int, days: pygismeteo_base.types.DAYS
     ) -> pygismeteo_base.models.step24.Model:
+        """Погода с шагом 24 часа.
+
+        Args:
+            id: ID населённого пункта.
+                Получить можно при помощи метода get_id_by_query.
+            days: Количество дней (от 3 до 10).
+        """
         return pygismeteo_base.models.step24.Model.parse_obj(
             self._get_response(
                 f"weather/forecast/aggregate/{id}", {"days": str(days)}
@@ -52,6 +90,17 @@ class Gismeteo:
         )
 
     def get_id_by_query(self, query: str) -> int:
+        """Получение ID населённого пункта по названию.
+
+        Args:
+            query: Название населённого пункта.
+
+        Raises:
+            LocalityNotFound: Населённый пункт не найден.
+
+        Returns:
+            ID населённого пункта.
+        """
         items = self._get_response("search/cities", {"query": query})["items"]
         if not items:
             raise LocalityNotFound("Населённый пункт не найден.")

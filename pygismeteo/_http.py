16a7c6e8514b912bb1869040c87b2cdd44ef3697
monosans <hsyqixco@protonmail.com>
2022-07-10 21:23:56 +0300

    Add `from __future__ import annotations`

--- pygismeteo/_http.py
+++ pygismeteo/_http.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from typing import Any, Optional
 
 from pygismeteo_base.http import BaseHttpClient
--- tests/test_everything.py
+++ tests/test_everything.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from requests import Session
 
 from pygismeteo import Gismeteo
